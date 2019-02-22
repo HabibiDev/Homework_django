d7c9975abf7984a67232c1d818ba454876459823
Jarvis <belitskeya@gmail.com>
2019-02-22 20:10:06 +0200

    Create Serializers

--- coocking/coocking_book/serializers.py
+++ coocking/coocking_book/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 from .models import Dish, Order
 
+
 class DishSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Dish
-        fields = ('id', 'title', 'description')
+        fields = ('id', 'title', 'description', 'author')
+
 
 class OrderSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Order
-        fields = ('id', 'contact', 'order_date', 'is_active')
+        fields = ('id', 'contact', 'order_date', 'is_active', 'dish', 'author')
